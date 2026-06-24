ef674e1e9f3fe8ffc521d42a0607b92cc9e80d5e
miqueridamia <mwojtala0202@gmail.com>
2026-06-24 20:11:09 +0200

    config cypress for frontend tests and add few simple tests

---
+++ frontend/cypress/e2e/front_tests.cy.js
@@ -0,0 +1,29 @@
+describe("Aplikacja Steganograficzna", () => {
+  it("powinna poprawnie załadować stronę główną", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("body").should("exist");
+  });
+
+  it("powinna zablokować kodowanie bez wybranego pliku", () => {
+    cy.visit("http://localhost:5173");
+
+    cy.get("textarea").type("To jest testowa wiadomość od Cypressa");
+
+    cy.get("button").contains("Zakoduj").click();
+
+    cy.contains("Wybierz plik").should("be.visible");
+  });
+
+  it("powinna załadować plik bmp, wpisać tekst i rozpocząć kodowanie", () => {
+    cy.visit("http://localhost:5173");
+
+    cy.get('input[type="file"]').selectFile(
+      "cypress/fixtures/sample_640x480.bmp",
+      { force: true },
+    );
+
+    cy.get("textarea").type("To jest sekretna wiadomość dla Cypressa");
+    cy.get("button").contains("Zakoduj").click();
+    cy.get("body").should("not.contain", "Wybierz plik");
+  });
+});
